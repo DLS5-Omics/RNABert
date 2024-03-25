73d16341482fd05ba62b26ddb60719a365479080
Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>
2024-03-25 16:52:14 +0800

    register AutoClass in loading rnabert

--- rnabert/__init__.py
+++ rnabert/__init__.py
@@ -1,5 +1,12 @@
+from transformers import AutoConfig, AutoModel, AutoTokenizer
+
 from .configuration_rnabert import RnaBertConfig
 from .modeling_rnabert import RnaBertModel
 from .tokenization_rnabert import RnaBertTokenizer
 
 __all__ = ["RnaBertConfig", "RnaBertModel", "RnaBertTokenizer"]
+
+
+AutoConfig.register("rnabert", RnaBertConfig)
+AutoModel.register(RnaBertConfig, RnaBertModel)
+AutoTokenizer.register(RnaBertConfig, RnaBertTokenizer)
